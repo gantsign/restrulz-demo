2fc36e8f8f07463c12454fd72f07b2d05126a4cb
John Freeman <john.freeman.opensource@gmail.com>
2017-01-03 19:57:38 +0000

    Initial commit after running Maven archetype
    Initial commit after running `com.github.gantsign.java-application-maven-archetype:java-application-maven-archetype`.

---
+++ src/main/java/com/gantsign/restrulz/demo/Main.java
@@ -0,0 +1,14 @@
+package com.gantsign.restrulz.demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Description of class.
+ */
+@Slf4j
+public class Main {
+
+  public static void main(String[] args) {
+    log.info("Hello, World!");
+  }
+}
